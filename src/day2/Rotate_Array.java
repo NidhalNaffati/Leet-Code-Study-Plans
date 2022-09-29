758b274e1bfbee704e2aa64b72ff49cec39faed1
ryuke <nidhalnaffati@gmail.com>
2022-09-29 17:30:54 +0100

    create translateArrayOneIndexToTheRight method

--- src/day2/Rotate_Array.java
+++ src/day2/Rotate_Array.java
@@ -7,14 +7,17 @@ public class Rotate_Array {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
         System.out.println();
-        permuteArray(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
+       translateArrayOneIndexToTheRight(nums,0);
+       permuteArray(nums);
+
+        for (int j = 0; j < nums.length ; j++) {
+            System.out.print(nums[j] + " ");
         }
+
     }
 
     public static void permuteArray(int[] nums) {
@@ -29,6 +32,9 @@ public class Rotate_Array {
 
         nums[0] = temp;
 
+
+
+
     }
 
     public static void rotate(int[] nums, int k) {
@@ -38,10 +44,14 @@ public class Rotate_Array {
 
     public static void translateArrayOneIndexToTheRight(int[] array , int indexOfStarting ){
 
+        int temp1 = array[indexOfStarting] ;
+        int temp2 ;
         for(int i = indexOfStarting ;i<array.length -1 ;i++){
-            int temp1 = array[i] ;
-            int temp2 = array[i+1];
+
+            temp2 = array[i+1];
             array[i+1]=temp1 ;
+            temp1=temp2;
+
         }
     }
 
